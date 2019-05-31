eab93ebf75e4a9ec8b9726db4d582572154dbb9a
chenhuihua <huihua.chen@bozhon.com>
2019-05-31 17:15:30 +0800

    awaitility demo

---
+++ src/main/java/com/example/demo/AwaitilityDemo.java
@@ -0,0 +1,29 @@
+package com.example.demo;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+import static org.awaitility.Awaitility.await;
+
+/**
+ * AwaitilityDemo
+ *
+ * @author Administrator
+ * @date 2019/5/31
+ */
+public class AwaitilityDemo {
+
+    public static void main(String[] args) {
+        final int[] a = {0};
+        Timer timer = new Timer();
+        timer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                a[0] += 1;
+                System.out.println(a[0]);
+            }
+        }, 0, 1000);
+        await().until(() -> a[0] == 5);
+        timer.cancel();
+    }
+}
