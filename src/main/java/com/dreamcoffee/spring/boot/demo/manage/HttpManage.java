5e4d4029ba281c791019c6cd4357693bd6689849
Frank_Chen <frank_chen@epam.com>
2020-08-06 09:36:54 +0800

    postJson generic

--- src/main/java/com/dreamcoffee/spring/boot/demo/manage/HttpManage.java
+++ src/main/java/com/dreamcoffee/spring/boot/demo/manage/HttpManage.java
@@ -3,6 +3,9 @@ package com.dreamcoffee.spring.boot.demo.manage;
 import com.alibaba.fastjson.JSONObject;
 import com.dreamcoffee.spring.boot.demo.Application;
 import com.dreamcoffee.spring.boot.demo.common.Constant;
+import com.dreamcoffee.spring.boot.demo.common.ResultDTO;
+import com.dreamcoffee.spring.boot.demo.customer.entity.Customer;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,11 +13,15 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
 import org.springframework.stereotype.Component;
 import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -63,6 +70,18 @@ public class HttpManage {
         return restTemplate.postForObject(url, args, String.class);
     }
 
+    /**
+     * Object -> application/json;charset=UTF-8
+     *
+     * @param url
+     * @param args
+     * @param responseType
+     * @return
+     */
+    public <T> T postJson(String url, JSONObject args, ParameterizedTypeReference<T> responseType) {
+        return restTemplate.exchange(url, HttpMethod.POST, new HttpEntity<>(args), responseType).getBody();
+    }
+
     public static void main(String[] args) {
         ApplicationContext ctx = SpringApplication.run(Application.class, args);
         HttpManage httpManage = ctx.getBean(HttpManage.class);
@@ -70,6 +89,8 @@ public class HttpManage {
         jsonObject.put("name", "John");
         String result = httpManage.postJson(Constant.TEST_HTTP_URL, jsonObject);
         LOGGER.info(result);
+        ResultDTO<List<Customer>> res = httpManage.postJson(Constant.TEST_HTTP_URL, jsonObject, new ParameterizedTypeReference<ResultDTO<List<Customer>>>() {});
+        System.out.println(res);
         System.exit(0);
     }
 
